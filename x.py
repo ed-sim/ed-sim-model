0f6e9293b82a4ac599da62bad6f2ff9cbe16efe8
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-07-15 17:27:27 +0100

    Simulate based on actual time of day and day of week data

--- x.py
+++ x.py
@@ -1,11 +1,12 @@
+from __future__ import print_function
 import pprint
 import random
 
 class Patient:
-    def __init__(self, priority, treatment_time):
+    def __init__(self, priority, treatment_duration):
         assert 0 <= priority <= 5
         self.priority = priority
-        self.treatment_time = treatment_time
+        self.treatment_duration = treatment_duration
         self.arrived = None
         self.seen = None
         self.finished = None
@@ -18,18 +19,41 @@ class Doctor:
     def __init__(self):
         self.patient = None
 
+weekarrivals = 5000
+reldayattends = [
+    0.158055144,0.141951248,0.141242013,0.139010815,0.134600165,
+    0.136450873,0.148717898
+]
+relhourattends = [
+    0.021955416,0.017915919,0.014026032,0.012211999,
+    0.01168836,0.010267056,0.012679533,0.018626571,
+    0.034204817,0.054907241,0.059489078,0.065380012,
+    0.06399611,0.063491173,0.06059246,0.06291143,
+    0.064239228,0.06046155,0.064257929,0.060480251,
+    0.054084381,0.044939408,0.037514961,0.029679084,
+]
+patientqueue = []
+for day in range(7):
+    for hour in range(24):
+        numarrivals = int(weekarrivals * reldayattends[day] * relhourattends[hour])
+        for _ in range(numarrivals):
+            arrivaltime = round((24*60*day) + (60*hour) + (random.uniform(0, 1)*59))
+            patientqueue.append(arrivaltime)
+patientqueue.sort()
+
 def patients_in_minute(minute):
-    if random.randint(0, 1) == 0:
-        return []
-    else:
+    patients = []
+    while patientqueue and patientqueue[0] == minute:
+        arrivaltime = patientqueue.pop(0)
         priority = random.randint(0, 5)
-        treatment_time = random.normalvariate(20, 10)
-        patient = Patient(priority, treatment_time)
+        treatment_duration = max(random.normalvariate(20, 20), 1)
+        patient = Patient(priority, treatment_duration)
         patient.arrived = minute
-        return [patient]
+        patients.append(patient)
+    return patients
 
 def did_patient_finish_with_doctor(minute, patient):
-    return minute - patient.seen >= patient.treatment_time
+    return minute - patient.seen >= patient.treatment_duration
 
 def sim_minute(minute, patients, finished_patients, doctors):
     patients.extend(patients_in_minute(minute))
@@ -50,17 +74,17 @@ def sim_minute(minute, patients, finished_patients, doctors):
 def go():
     patients = []
     finished_patients = []
-    doctors = [Doctor() for i in range(7)]
+    doctors = [Doctor() for i in range(12)]
 
-    # Warmup
-    for minute in range(200):
+    # TODO: add warmup - queue isn't empty at beginning of day
+    for minute in range(0, 7*24*60):
+        if minute % 60 == 0:
+            print('At minute {}, {} patients in queue, {} finished patients'.format(
+                minute, len(patients), len(finished_patients)
+            ))
         sim_minute(minute, patients, finished_patients, doctors)
-    finished_patients = []
 
-    for minute in range(201, 250):
-        sim_minute(minute, patients, finished_patients, doctors)
-
-    pprint.pprint(finished_patients)
+    print('Saw {} patients, {} remaining'.format(len(finished_patients), len(patients)))
 
 if __name__ == '__main__':
     go()
