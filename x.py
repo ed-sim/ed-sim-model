5a262379621ed947c4d6bd57f67993c6bb90c669
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-07-16 11:37:30 +0100

    blood, xray

--- x.py
+++ x.py
@@ -1,23 +1,7 @@
 from __future__ import print_function
+import heapq
 import pprint
 import random
-
-class Patient:
-    def __init__(self, priority, treatment_duration):
-        assert 0 <= priority <= 5
-        self.priority = priority
-        self.treatment_duration = treatment_duration
-        self.arrived = None
-        self.seen = None
-        self.finished = None
-    def __repr__(self):
-        return 'Patient(priority={},arrived={},seen={},finished={})'.format(
-                self.priority, self.arrived, self.seen, self.finished
-        )
-
-class Doctor:
-    def __init__(self):
-        self.patient = None
 
 weekarrivals = 5000
 reldayattends = [
@@ -32,59 +16,168 @@ relhourattends = [
     0.064239228,0.06046155,0.064257929,0.060480251,
     0.054084381,0.044939408,0.037514961,0.029679084,
 ]
-patientqueue = []
+
+NUM_DOCTORS = 12
+NUM_BLOODS = 2
+NUM_XRAYS = 1
+BLOOD_PROB = 0.2
+XRAY_PROB = 0.1
+
+def cmp(a, b):
+    return (a > b) - (a < b)
+# mixin class for Python3 supporting __cmp__
+class PY3__cmp__:
+    def __eq__(self, other):
+        return self.__cmp__(other) == 0
+    def __ne__(self, other):
+        return self.__cmp__(other) != 0
+    def __gt__(self, other):
+        return self.__cmp__(other) > 0
+    def __lt__(self, other):
+        return self.__cmp__(other) < 0
+    def __ge__(self, other):
+        return self.__cmp__(other) >= 0
+    def __le__(self, other):
+        return self.__cmp__(other) <= 0
+
+def gen_doctor_duration():
+    return int(max(random.normalvariate(20, 20), 1))
+def gen_blood_duration():
+    if random.uniform(0, 1) > BLOOD_PROB:
+        return None
+    return int(max(random.normalvariate(20, 20), 10))
+def gen_xray_duration():
+    if random.uniform(0, 1) > XRAY_PROB:
+        return None
+    return int(max(random.normalvariate(20, 20), 20))
+
+class Patient(PY3__cmp__):
+    def __init__(self, priority, doctor_duration, blood_duration, xray_duration):
+        assert 1 <= priority <= 5
+        self.priority = priority
+        self.doctor_duration = doctor_duration
+        self.blood_duration = blood_duration
+        self.xray_duration = xray_duration
+        self.arrived = None
+        self.started_doctor = None
+        self.started_blood = None
+        self.started_xray = None
+        self.finished = None
+    def __repr__(self):
+        return 'Patient(priority={})'.format(self.priority)
+    def __cmp__(self, other):
+        return self.priority - other.priority
+    def finished_with_doctor(self, minute):
+        return minute - self.started_doctor == self.doctor_duration
+    def finished_with_blood(self, minute):
+        return minute - self.started_blood == self.blood_duration
+    def finished_with_xray(self, minute):
+        return minute - self.started_xray == self.xray_duration
+
+class Slots:
+    def __init__(self, total_num):
+        self.__total_num = total_num
+        self.current_patients = []
+    def have_free(self):
+        return len(self.current_patients) < self.__total_num
+
+PATIENT_ARRIVALS = []
 for day in range(7):
     for hour in range(24):
         numarrivals = int(weekarrivals * reldayattends[day] * relhourattends[hour])
         for _ in range(numarrivals):
             arrivaltime = round((24*60*day) + (60*hour) + (random.uniform(0, 1)*59))
-            patientqueue.append(arrivaltime)
-patientqueue.sort()
+            PATIENT_ARRIVALS.append(arrivaltime)
+PATIENT_ARRIVALS.sort(reverse=True)
 
 def patients_in_minute(minute):
     patients = []
-    while patientqueue and patientqueue[0] == minute:
-        arrivaltime = patientqueue.pop(0)
-        priority = random.randint(0, 5)
-        treatment_duration = max(random.normalvariate(20, 20), 1)
-        patient = Patient(priority, treatment_duration)
+    while PATIENT_ARRIVALS and PATIENT_ARRIVALS[-1] == minute:
+        arrivaltime = PATIENT_ARRIVALS.pop()
+
+        priority = random.randint(1, 5)
+        doctor_duration = gen_doctor_duration()
+        blood_duration = gen_blood_duration()
+        xray_duration = gen_xray_duration()
+
+        patient = Patient(priority, doctor_duration, blood_duration, xray_duration)
         patient.arrived = minute
         patients.append(patient)
     return patients
 
-def did_patient_finish_with_doctor(minute, patient):
-    return minute - patient.seen >= patient.treatment_duration
+def transition_patient(patient, doctor_queue, blood_queue, xray_queue, finished_patients):
+    assert patient.doctor_duration is not None and patient.started_doctor is not None
+    if patient.blood_duration and not patient.started_blood:
+        blood_queue.append(patient)
+    elif patient.xray_duration and not patient.started_xray:
+        xray_queue.append(patient)
+    else:
+        finished_patients.append(patient)
 
-def sim_minute(minute, patients, finished_patients, doctors):
-    patients.extend(patients_in_minute(minute))
-    patients.sort(key=lambda p: p.priority)
+def sim_minute(minute, doctor_queue, blood_queue, xray_queue, finished_patients, doctor_slots, blood_slots, xray_slots):
+    doctor_queue.extend(patients_in_minute(minute))
+    doctor_queue.sort()
 
-    for doctor in doctors:
-        if doctor.patient is None and len(patients):
-            doctor.patient = patients.pop()
-            doctor.patient.seen = minute
-        if doctor.patient is None:
-            continue
-        patient = doctor.patient
-        if did_patient_finish_with_doctor(minute, patient):
-            patient.finished = minute
-            finished_patients.append(patient)
-            doctor.patient = None
+    while doctor_slots.have_free() and len(doctor_queue):
+        patient = doctor_queue.pop()
+        patient.started_doctor = minute
+        doctor_slots.current_patients.append(patient)
+    new_current_patients = []
+    for patient in doctor_slots.current_patients:
+        if patient.finished_with_doctor(minute):
+            transition_patient(patient, doctor_queue, blood_queue, xray_queue, finished_patients)
+        else:
+            new_current_patients.append(patient)
+    doctor_slots.current_patients = new_current_patients
+
+    while blood_slots.have_free() and len(blood_queue):
+        patient = blood_queue.pop()
+        patient.started_blood = minute
+        blood_slots.current_patients.append(patient)
+    new_current_patients = []
+    for patient in blood_slots.current_patients:
+        if patient.finished_with_blood(minute):
+            transition_patient(patient, doctor_queue, blood_queue, xray_queue, finished_patients)
+        else:
+            new_current_patients.append(patient)
+    blood_slots.current_patients = new_current_patients
+
+    while xray_slots.have_free() and len(xray_queue):
+        patient = xray_queue.pop()
+        patient.started_xray = minute
+        xray_slots.current_patients.append(patient)
+    new_current_patients = []
+    for patient in xray_slots.current_patients:
+        if patient.finished_with_xray(minute):
+            transition_patient(patient, doctor_queue, xray_queue, xray_queue, finished_patients)
+        else:
+            new_current_patients.append(patient)
+    xray_slots.current_patients = new_current_patients
 
 def go():
-    patients = []
+    doctor_queue = []
+    blood_queue = []
+    xray_queue = []
     finished_patients = []
-    doctors = [Doctor() for i in range(12)]
+    doctor_slots = Slots(NUM_DOCTORS)
+    blood_slots = Slots(NUM_BLOODS)
+    xray_slots = Slots(NUM_XRAYS)
 
     # TODO: add warmup - queue isn't empty at beginning of day
     for minute in range(0, 7*24*60):
         if minute % 60 == 0:
-            print('At minute {}, {} patients in queue, {} finished patients'.format(
-                minute, len(patients), len(finished_patients)
+            print('At minute {}, DQ:{}, BQ:{}, XQ:{}, FIN:{}'.format(
+                minute, len(doctor_queue), len(blood_queue), len(xray_queue), len(finished_patients)
             ))
-        sim_minute(minute, patients, finished_patients, doctors)
+        sim_minute(minute, doctor_queue, blood_queue, xray_queue, finished_patients, doctor_slots, blood_slots, xray_slots)
 
-    print('Saw {} patients, {} remaining'.format(len(finished_patients), len(patients)))
+    print('Saw {} patients'.format(len(finished_patients)))
 
 if __name__ == '__main__':
-    go()
+    try:
+        go()
+    except:
+        import pdb, sys, traceback
+        type, value, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
